15bb03d9dc8f9768f6f927e1e8dc3f3ccb783acd
nerik <erik.escoffier@gmail.com>
2015-12-21 16:50:38 +0100

    trump trump trump

--- trump_data/wikipedia.js
+++ trump_data/wikipedia.js
@@ -4,22 +4,27 @@ $('.wikitable').each(function(i, table) {
 
    $t = $(table);
 
+   $t.find('td:nth-child(n+4)').each(function(j, td) {
+      if (!$(td).attr('style')) {
+          $(td).text('');
+      }
+   });
+
    $t.find('td').removeAttr( 'rowspan' )
 
    $t.find('tr').each(function(k, tr) {
       if ($(tr).find('td').length===4) {
          $(tr).remove()
       }
+      var text = $(tr).find('td:nth-child(n+4)').text();
    });
-    $t.find('td:nth-child(n+4)').each(function(j, td) {
-      if (!$(td).attr('style')) {
-          $(td).text('');
-      }
-   });
+
 
    $t.find('tr').prepend('<td>'+state+'</td>');
    $t.find('th').first().prev().replaceWith('<th>state</th>');
 
+});
 
+$('#mw-content-text > *:not(table)').remove();
 
-})
+console.log($('#mw-content-text').html())
